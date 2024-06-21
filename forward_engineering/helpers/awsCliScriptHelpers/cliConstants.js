5380267532a9a27a5e7865891c640957b7c73b75
Teamcity <teamcity@hackolade.com>
2024-06-21 17:46:49 +0000

    Release: 0.2.1

--- forward_engineering/helpers/awsCliScriptHelpers/cliConstants.js
+++
@@ -1,9 +0,0 @@
-const CLI = 'aws glue';
-const CREATE_DATABASE = 'create-database --cli-input-json';
-const CREATE_TABLE = 'create-table --cli-input-json';
-
-module.exports = {
-	CLI,
-	CREATE_DATABASE,
-	CREATE_TABLE
-};
