d1f39503f950b08fa6bfdf312e00bf174258fca6
chulanovskyi <oleg.chulanovskyi@binary-studio.com>
2024-08-30 13:38:40 +0300

    fix: preserve error trace in reject

--- reverse_engineering/api.js
+++ reverse_engineering/api.js
@@ -169,7 +169,7 @@ const handleFileData = filePath => {
 	return new Promise((resolve, reject) => {
 		fs.readFile(filePath, 'utf-8', (err, content) => {
 			if (err) {
-				reject(new Error(`handle file data error: ${err}`));
+				reject(err);
 			} else {
 				resolve(content);
 			}
