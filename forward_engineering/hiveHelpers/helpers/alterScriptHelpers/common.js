e64d8a6a2f82b756ba1bf19ec83bd76f06531ea3
Alik Rakhmonov <alik.rahmonov@gmail.com>
2025-12-19 09:28:06 +0100

    HCK-14053: parse table properties in alter

--- forward_engineering/hiveHelpers/helpers/alterScriptHelpers/common.js
+++ forward_engineering/hiveHelpers/helpers/alterScriptHelpers/common.js
@@ -9,25 +9,38 @@ const getDifferentItems = (newItems = [], oldItems = []) => {
 };
 
 const hydrateTableProperties = ({ new: newItems, old: oldItems }, name, commentState) => {
-	const hydrateProperties = properties => (properties || '').split(',').map(prop => prop.trim());
 	const prepareProperties = properties =>
 		properties
 			.filter(Boolean)
-			.map(property => property.replace(/(\S+)=([\s\S]+)/, `'$1'='$2'`))
+			.map(property => `'${property.tablePropKey}'='${property.tablePropValue}'`)
 			.join(',\n');
 
 	const isCommentChanged = !_.isEqual(commentState?.new, commentState?.old);
-	const addCommentProp = isCommentChanged && commentState?.new ? `comment=${commentState?.new}` : '';
-	const dropCommentProp = isCommentChanged && !commentState?.new ? `comment` : '';
+
+	const addCommentProp =
+		isCommentChanged && commentState?.new
+			? {
+					tablePropKey: 'comment',
+					tablePropValue: commentState?.new,
+				}
+			: null;
+
+	const dropCommentProp =
+		isCommentChanged && !commentState?.new
+			? {
+					tablePropKey: 'comment',
+				}
+			: null;
 
 	const preparePropertiesName = properties =>
 		properties
 			.filter(Boolean)
-			.map(prop => `'${prop.replace(/(=\S+)/, '')}'`)
+			.map(prop => `'${prop.tablePropKey}'`)
 			.join(', ');
 
-	const newHydrateItems = hydrateProperties(newItems);
-	const oldHydrateItems = hydrateProperties(oldItems);
+	// Ensure newItems and oldItems are arrays
+	const newHydrateItems = Array.isArray(newItems) ? newItems : [];
+	const oldHydrateItems = Array.isArray(oldItems) ? oldItems : [];
 
 	const { add, drop } = getDifferentItems(newHydrateItems, oldHydrateItems);
 
